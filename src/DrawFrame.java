ea4e670a2c0185384ce057bfb352d82545afedfa
TomBenShmueli <tombenshmueli@gmail.com>
2020-05-01 14:23:37 +0300

    Initial commit

---
+++ src/DrawFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class DrawFrame extends JFrame
+{
+    JFrame frame;
+    JPanel buttonPanel;
+    JPanel drawPanel;
+    JButton exitButton, undoButton, clearButton , lineButton, rectangleButton , roundRectangleButton , ovalButton;
+    JCheckBox isFilledBox;
+
+    public void CreateAndRunGUI()
+    {
+        frame = new JFrame();
+
+    }
+
+
+}
